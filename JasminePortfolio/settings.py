d24c2bc29876500a733bce9b45ad0edb666c9d49
hschafer2017 <hschafer@umich.edu>
2018-09-24 21:20:50 +0100

    CDNs loaded and static files hooked up

--- JasminePortfolio/settings.py
+++ JasminePortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uffnzwbc@sop8d!g=0wl%y#d**ol@%=+8z7^8rzjd*++obx-s6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
